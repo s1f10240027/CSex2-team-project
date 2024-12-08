ff0314fcb633b76c3ab8765b955fbbd3c59eda68
s1f10240027 <s1f102400279@iniad.org>
2024-12-08 19:47:14 +0900

    musiq内のurlsを参照するように設定

--- django1/config/urls.py
+++ django1/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from musiq import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("musiq.urls")),
 ]
