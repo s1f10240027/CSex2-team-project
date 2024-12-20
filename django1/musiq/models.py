c59abc6c68ecea3c481d8c5ad2db57e1ad849045
s1f10240027 <s1f102400279@iniad.org>
2024-12-20 12:52:02 +0900

    ゲーム中のデータ管理

--- django1/musiq/models.py
+++ django1/musiq/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class GameSession(models.Model):
+    session_id = models.IntegerField(unique=True)      #乱数(リザルトで非ログインの人も登録可にするために)
+    current_question = models.IntegerField(default=0)         # 終了問題数
+    correct = models.IntegerField(default=0)        # 正解数
+    answer_times = models.JSONField(default=list)   # 回答時間
+    max_streak = models.IntegerField(default=0)     # 最高連続正解数
+    streak = models.IntegerField(default=0)         # 現在の連続正解数
+    genre = models.CharField(max_length=100, default="All")
