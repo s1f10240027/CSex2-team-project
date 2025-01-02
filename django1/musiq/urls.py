89ba9c42168ac3a811bd1df46f80c965309a4868
s1f10240027 <s1f102400279@iniad.org>
2025-01-03 04:46:19 +0900

    ランキングとルール画面に遷移できるように

--- django1/musiq/urls.py
+++ django1/musiq/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path("Mypage/rename", views.rename, name="rename"),
     path("result", views.result, name="result"),
     path("retry", views.retry, name="retry"),
+    path("Info", views.rules, name="rules"),
     path("spotify", views.CheckSpotify, name="CheckSpotify"),
 ]
