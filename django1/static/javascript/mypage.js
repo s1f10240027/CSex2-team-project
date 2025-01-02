7c4d56464e044e85505a568ca48413483062381e
s1f10240027 <s1f102400279@iniad.org>
2025-01-02 20:59:33 +0900

    mypageで音楽を再生
    renameと共に

--- django1/static/javascript/mypage.js
+++ django1/static/javascript/mypage.js
@@ -1,11 +1,44 @@
+const audio = new Audio("/static/media/mypage.mp3");
+
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('last_page', window.location.pathname);
+    if (!(audio.paused)) {
+        localStorage.setItem('mypage_currentTime', audio.currentTime);
+    };
+})
+
+//2ページ共通 bgmの再生
+document.addEventListener('DOMContentLoaded', () => {
+    const isOnMypage = `${localStorage.getItem('last_page')}`.includes("Mypage");
+    const savedTime = parseFloat(localStorage.getItem('mypage_currentTime'));
+
+    audio.addEventListener('loadedmetadata', () => {
+        audio.currentTime = 0;
+        if (isOnMypage && !isNaN(savedTime)) {
+            audio.currentTime = savedTime + 0.1;
+        } else {
+            localStorage.removeItem('mypage_currentTime');
+        }
+        const isPlaying = localStorage.getItem('musicPlaying');
+        if (isPlaying === 'true') {
+            audio.loop = true;
+            audio.play();
+        };
+    })
+})
 
 function changeIcon() {
     // アイコン変更処理も今後追加予定
+    console.log(audio)
+    audio.currentTime = 10;
     alert("アイコン変更機能が未実装です。");
 }
 
-
+// renameの制限処理
 document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname != '/Mypage/rename') {
+        return;
+    };
     const usernameInput = document.querySelector("input[name='name']");
     const usernameError = document.getElementById('name_error');
     const button = document.querySelector(".change");
