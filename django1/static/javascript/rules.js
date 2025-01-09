32af2cd9f019327f84a70646f9de1af646adaf42
s1f10240027 <s1f102400279@iniad.org>
2025-01-10 01:10:31 +0900

    音楽の追加

---
+++ django1/static/javascript/rules.js
@@ -0,0 +1,28 @@
+// 音楽再生処理
+const audio = new Audio("/static/media/bgm_rules.mp3");
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('last_page', window.location.pathname);
+    if (!(audio.paused)) {
+        localStorage.setItem('ranking_currentTime', audio.currentTime);
+    };
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const isOnRanking = `${localStorage.getItem('last_page')}`.includes("Ranking");
+    const savedTime = parseFloat(localStorage.getItem('ranking_currentTime'));
+
+    audio.addEventListener('loadedmetadata', () => {
+        audio.currentTime = 0;
+        if (isOnRanking && !isNaN(savedTime)) {
+            audio.currentTime = savedTime + 0.1;
+        } else {
+            localStorage.removeItem('ranking_currentTime');
+        }
+        const isPlaying = localStorage.getItem('musicPlaying');
+        if (isPlaying === 'true') {
+            console.log("audio started")
+            audio.loop = true;
+            audio.play();
+        };
+    })
+})
